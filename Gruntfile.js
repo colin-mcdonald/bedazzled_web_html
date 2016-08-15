3245f3d21901690e6a0ac453adb1913d229a1a31
Kevin Caccamo <kevin@ciinet.org>
2016-08-14 22:21:40 -0400

    Make JSLint happy.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,10 @@
-module.exports = function(grunt) {
-  grunt.initConfig({
-    less: {
-      'bedazzled.css': ['bedazzled.less']
-    }
-  });
+module.exports = function (grunt) {
+    grunt.initConfig({
+        less: {
+            'bedazzled.css': ['bedazzled.less']
+        }
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.registerTask('default', ['less']);
-}
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.registerTask('default', ['less']);
+};
