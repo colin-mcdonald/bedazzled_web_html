377963b0ca3d4fa87e8e27d8616b842598b94d31
Kevin Caccamo <kevin@ciinet.org>
2016-08-15 04:34:28 -0400

    More work on the build system
    Gruntfile now uses the less plugin as it should be used.
    Move bedazzled.less into its own folder.
    Change license to specify that it is copyrighted.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,39 @@
 module.exports = function (grunt) {
     grunt.initConfig({
         less: {
-            'bedazzled.css': ['bedazzled.less']
+            development: {
+                options: {
+                    strictImports: true,
+                    strictMath: true,
+                    syncImport: true,
+                    relativeUrls: true,
+                    sourceMap: true,
+                    sourceMapFileInline: true,
+                    ieCompat: false,
+                    //plugins: ["autoprefix"],
+                    compress: false
+                },
+                files: {
+                    'cssd/bedazzled.css': ['less/bedazzled.less']
+                }
+            },
+            production: {
+                options: {
+                    strictImports: true,
+                    strictMath: true,
+                    syncImport: true,
+                    relativeUrls: true,
+                    sourceMap: false,
+                    sourceMapFileInline: false,
+                    ieCompat: true,
+                    //plugins: ["autoprefix"],
+                    compress: true
+                },
+                files: {
+                    'css/bedazzled.css': ['less/bedazzled.less']
+                }
+            }
+
         }
     });
 
